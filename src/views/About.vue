674e805faa92a8deef68b63315adfd0db47fe08e
liu <837238676@qq.com>
2021-01-22 23:52:49 +0800

    树结构

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,47 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div>
+        <div class="about">
+            <h1>This is an about page</h1>
+        </div>
+        <vue-okr-tree :data="testData"/>
+    </div>
 </template>
+
+<script>
+    import {VueOkrTree} from 'vue-okr-tree';
+    import 'vue-okr-tree/dist/vue-okr-tree.css'
+
+    export default {
+        name: '',
+        components: {
+            VueOkrTree
+        },
+        data() {
+            return {
+                testData: [{
+                    label: 'xxx科技有有限公司',
+                    children: [{
+                        label: '产品研发部',
+                        children: [{
+                            label: '研发-前端',
+                        }, {
+                            label: '研发-后端',
+                        }, {
+                            label: 'UI 设计',
+                        }]
+                    }, {
+                        label: '销售部',
+                        children: [{
+                            label: '销售一部',
+                        }, {
+                            label: '销售二部',
+                        }
+                        ]
+                    }, {
+                        label: '财务部'
+                    }]
+                }]
+            }
+        }
+    }
+</script>
